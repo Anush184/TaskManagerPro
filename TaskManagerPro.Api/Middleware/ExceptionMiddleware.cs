addb65333f892a883b0a3478be41c84e3fad613c
Anush Avetisyan <anavettt@gmail.com>
2023-11-05 23:38:56 +0400

    Fix-05.11.23

--- TaskManagerPro.Api/Middleware/ExceptionMiddleware.cs
+++ TaskManagerPro.Api/Middleware/ExceptionMiddleware.cs
@@ -7,73 +7,72 @@ using TaskManagerPro.Application.Exceptions;
 
 
 
-namespace TaskManagerPro.Api.Middleware
+namespace TaskManagerPro.Api.Middleware;
+
+public class ExceptionMiddleware
 {
-    public class ExceptionMiddleware
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
     {
-        private readonly RequestDelegate _next;
-
-        public ExceptionMiddleware(RequestDelegate next)
+        this._next = next;
+    }
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
         {
-            this._next = next;
+            await _next(httpContext);
         }
-        public async Task InvokeAsync(HttpContext httpContext)
+        catch(Exception ex) 
         {
-            try
-            {
-                await _next(httpContext);
-            }
-            catch(Exception ex) 
-            {
-                await HandleExceptionAsync(httpContext, ex);
-            }
-
+            await HandleExceptionAsync(httpContext, ex);
         }
 
-        private async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+    }
+
+    private async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+    {
+        HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+        object problem;
+
+        switch (ex)
         {
-            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
-            object problem;
-
-            switch (ex)
-            {
-                case BadRequestException badRequestException:
-                    statusCode = HttpStatusCode.BadRequest;
-                    problem = new CustomProblemDetails
-                    {
-                        Title = badRequestException.Message,
-                        Status = (int)statusCode,
-                        Detail = badRequestException.InnerException?.Message,
-                        Type = nameof(BadRequestException),
-                        Errors = badRequestException.ValidationErrors
-                    };
-                    break;
-                case NotFoundException notFoundException:
-                    statusCode = HttpStatusCode.NotFound;
-                    problem = new CustomProblemDetails
-                    {
-                        Title = notFoundException.Message,
-                        Status = (int)statusCode,
-                        Type = nameof(NotFoundException),
-                        Detail = ex.InnerException?.Message,
-                    };
-                    break;
-                default:
-                    problem = new CustomProblemDetails
-                    {
-                        Title = ex.Message,
-                        Status = (int)statusCode,
-                        Type = nameof(HttpStatusCode.InternalServerError),
-                        Detail = ex.StackTrace,
-
-                    };
-
+            case BadRequestException badRequestException:
+                statusCode = HttpStatusCode.BadRequest;
+                problem = new CustomProblemDetails
+                {
+                    Title = badRequestException.Message,
+                    Status = (int)statusCode,
+                    Detail = badRequestException.InnerException?.Message,
+                    Type = nameof(BadRequestException),
+                    Errors = badRequestException.ValidationErrors
+                };
                 break;
-            }
+            case NotFoundException notFoundException:
+                statusCode = HttpStatusCode.NotFound;
+                problem = new CustomProblemDetails
+                {
+                    Title = notFoundException.Message,
+                    Status = (int)statusCode,
+                    Type = nameof(NotFoundException),
+                    Detail = ex.InnerException?.Message,
+                };
+                break;
+            default:
+                problem = new CustomProblemDetails
+                {
+                    Title = ex.Message,
+                    Status = (int)statusCode,
+                    Type = nameof(HttpStatusCode.InternalServerError),
+                    Detail = ex.StackTrace,
 
-            httpContext.Response.StatusCode = (int)statusCode;
-            await httpContext.Response.WriteAsJsonAsync(problem);
+                };
 
+            break;
         }
+
+        httpContext.Response.StatusCode = (int)statusCode;
+        await httpContext.Response.WriteAsJsonAsync(problem);
+
     }
 }
